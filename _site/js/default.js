a5154d51a213aac39fe34de13d3ab40f4b15e6a5
II-Matto <wszbupteducn@126.com>
2013-05-12 21:59:11 +0800

    test

--- _site/js/default.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	$('pre').addClass('prettyprint linenums').attr('style', 'width:60%;margin:auto;overflow:auto;border-radius:5px');
-	$(document).ready(function() {
-		prettyPrint();
-	})
-})();
