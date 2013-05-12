d2ba6e0d3c68036fff72c6eabc5f7f4a8e2e29f0
II-Matto <wszbupteducn@126.com>
2013-05-12 21:32:06 +0800

    commit

---
+++ _site/js/default.js
@@ -0,0 +1,6 @@
+(function() {
+	$('pre').addClass('prettyprint linenums').attr('style', 'width:60%;margin:auto;overflow:auto;border-radius:5px');
+	$(document).ready(function() {
+		prettyPrint();
+	})
+})();
