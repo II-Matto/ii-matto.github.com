0199d3807fe248784b3e6d59d173b734206be660
II-Matto <wszbupteducn@126.com>
2015-02-15 00:07:04 +0800

    Restructure the folders.

---
+++ _site/bak/js/default.js
@@ -0,0 +1,6 @@
+(function() {
+	$('pre').addClass('prettyprint linenums').attr('style', 'width:80%;margin:auto;overflow:auto;border-radius:5px');
+	$(document).ready(function() {
+		prettyPrint();
+	})
+})();
