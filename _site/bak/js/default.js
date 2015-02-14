27aa0f3165425f1aa953395b1a9516b4c1ef0ae9
II-Matto <wszbupteducn@126.com>
2015-02-15 00:08:42 +0800

    Remove cached.

--- _site/bak/js/default.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	$('pre').addClass('prettyprint linenums').attr('style', 'width:80%;margin:auto;overflow:auto;border-radius:5px');
-	$(document).ready(function() {
-		prettyPrint();
-	})
-})();
